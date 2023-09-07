46a8d9045f585925b1283735419e10998d9d01e6
DavidDuarteC <davidandrex1390@gmail.com>
2023-09-06 21:14:34 -0500

    Update index and create login

---
+++ pages/login.js
@@ -0,0 +1,15 @@
+function IndextPage() {
+    return (
+        <div>
+            <form>
+                <input type="email" placeholder="ejemplo@gmail.com"></input>
+                <input type="password" placeholder="password"></input>
+                <button>
+                    Login
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default IndextPage
